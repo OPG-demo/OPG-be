401d737a0d1deae353ec215e4592f386b27eb1da
Jenn Soderborg <jsoderborg7@gmail.com>
2020-08-27 14:48:48 -0600

    added date parser to config

--- data/db-config.js
+++ data/db-config.js
@@ -1,5 +1,9 @@
 const knex = require('knex')
 const config = require('../knexfile')
+const { type } = require('os')
+
+const types = require('pg').types
+types.setTypeParser(1082, val => val)
 
 const dbEnv = process.env.DB_ENV || 'development'
 
